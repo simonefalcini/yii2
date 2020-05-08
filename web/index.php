532da57050c1ff82bfb48d089199e0bf834db472
Simone Falcini <simone@falcini.com>
2020-05-08 11:09:57 +0200

    updated index.php getenv YII_DEBUG

--- web/index.php
+++ web/index.php
@@ -1,12 +1,16 @@
 <?php
-
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+if ((bool)getenv('YII_DEBUG')===true) {
+	defined('YII_DEBUG') or define('YII_DEBUG', true);
+	defined('YII_ENV') or define('YII_ENV', 'dev');
+}
+else {
+	defined('YII_DEBUG') or define('YII_DEBUG', false);
+}
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require __DIR__ . '/../config/web.php';
+$config = require(__DIR__ . '/../config/web.php');
 
 (new yii\web\Application($config))->run();
